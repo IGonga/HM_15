d25760c4f4fb38705b216ae28b921616c4678040
Kiselov Vladyslav <kyselov.vladyslav.ig@gmail.com>
2026-02-01 15:18:37 +0200

    Update SpeedBoostItem.cs

--- Assets/Scripts/Item/SpeedBoostItem.cs
+++ Assets/Scripts/Item/SpeedBoostItem.cs
@@ -3,22 +3,17 @@ using UnityEngine;
 public class SpeedBoostItem : Item
 {
     [SerializeField] private float _valueModifire;
-    [SerializeField] private float _durationOfAction;
 
     private PlayerController _playerController;
-    private bool _isActive = false;
 
     public override void Use(GameObject target)
     {
         if (target.TryGetComponent<PlayerController>(out _playerController))
         {
-            _isActive = true;
             _playerController.ApplySpeedBoost(_valueModifire);
 
             DisplayInfo();
         }
-        else
-            Debug.Log("Не удалось использовать предмет.");
     }
 
     public override void DisplayInfo()
@@ -26,29 +21,10 @@ public class SpeedBoostItem : Item
         base.DisplayInfo();
 
         Debug.Log($"Скорость увеличена на: x{_valueModifire}");
-        Debug.Log($"Продолжительность: {_durationOfAction} сек.");
-    }
-
-    private void Update()
-    {
-        if (_isActive)
-        {
-            if (_durationOfAction < 0)
-            {
-                _playerController.TryGetComponent<PlayerController>(out _playerController); 
-                Dispose();
-            }
-
-            _durationOfAction -= Time.deltaTime;
-        }
     }
 
     public override void Dispose()
     {
-        _playerController.ResetSpeed();
-
-        Debug.Log($"Предмет деактивирован.");
-
         Destroy(gameObject);
     }
 }
